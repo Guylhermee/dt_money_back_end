0998d6aa291995cd23e75de4c4fa07bb179363f5
Guylhermee <guylhermee.b@hotmail.com>
2024-08-20 19:12:47 -0300

    Atualizar dto de criação de transação e adicionar dto de atualização de transação

--- src/dto/update-transaction.dto.ts
+++ src/dto/update-transaction.dto.ts
@@ -1,10 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { TransactionCategory } from '../enums/transaction-category';
 
-export class CreateTransactionDTO {
-  @ApiProperty({ example: 'Desenvolvimento de site' })
+export class UpdateTransactionDTO {
+  @ApiProperty({ example: 'Atualização do desenvolvimento de site' })
   title: string;
-  @ApiProperty({ example: 12000 })
+  @ApiProperty({ example: -1200 })
   price: number;
   @ApiProperty({ example: TransactionCategory.sale })
   category: TransactionCategory;
